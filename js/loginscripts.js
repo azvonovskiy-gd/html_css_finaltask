ca8127c2bb043d80c4052069a86387081c9f6520
eggrevolver <eggrevolver@gmail.com>
2017-08-21 14:04:33 +0300

    Initial branch commit Renamed some files with vague names

--- js/loginscripts.js
+++
@@ -1,7 +0,0 @@
-var modal = document.getElementById('modal-login-form');
-var overlay = document.getElementById('overlay');
-
-modal.onclick = function () {
-    if (!overlay.classList.contains('blur'))
-        overlay.classList.toggle('blur');
-};
